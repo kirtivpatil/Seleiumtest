ee10eaffe6adf10927e8167e14289f78c438e001
kirtivpatil <kirtivpatil@gmail.com>
2022-05-27 19:22:02 -0400

    From branch

--- src/test/java/grp1/Seleniumtest/test.java
+++ src/test/java/grp1/Seleniumtest/test.java
@@ -2,7 +2,7 @@ package grp1.Seleniumtest;
 
 public class test {
 	public static void main(String[] args) throws InterruptedException {
-		System.out.println("test1");
+		System.out.println("testbranch");
 		Thread.sleep(3000);
 	}
 
