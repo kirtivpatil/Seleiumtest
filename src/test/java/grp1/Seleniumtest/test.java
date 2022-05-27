f9064ad6754afc5ecc6bc70e331aaf987e604cc8
kirtivpatil <kirtivpatil@gmail.com>
2022-05-27 19:18:22 -0400

    branch1

--- src/test/java/grp1/Seleniumtest/test.java
+++ src/test/java/grp1/Seleniumtest/test.java
@@ -1,8 +1,9 @@
 package grp1.Seleniumtest;
 
 public class test {
-	public static void main(String[] args) {
-		System.out.println("test");
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("test1");
+		Thread.sleep(3000);
 	}
 
 }
